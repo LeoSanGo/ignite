35b8c84d4e4ae0fa5c447a309ef7a15eb786796d
leosango <souoleo@gmail.com>
2022-06-20 21:33:58 -0300

    chore: update jsx files to tsx files

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -3,14 +3,14 @@ import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
 
-const repository = {
-  name: 'unform',
-  description: 'Form in react',
-  link: 'https://github.com/unform/unform',
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
 };
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -22,8 +22,8 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        {repositories.map((repositories) => {
-          return <RepositoryItem key={repositories.name} repository={repositories} />;
+        {repositories.map((repository) => {
+          return <RepositoryItem key={repository.name} repository={repository} />;
         })}
       </ul>
     </section>
